fed6fd540837edc07daf25a857144cb5c69ace57
hyeonju <hyeonju@fnj.or.kr>
2024-03-27 00:03:04 +0900

    Board, Square 컴포넌트 생성

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Square from './Square'
+import "./Board.css";
+
+export default class Board extends Component {
+    renderSqaure(i){
+        return <Square />
+    }
+  render() {
+    return (
+      <div>
+        <div className='status'></div>
+        <div className='board-row'>
+            {this.renderSqaure(0)}
+            {this.renderSqaure(1)}
+            {this.renderSqaure(2)}
+        </div>
+        <div className='board-row'>
+            {this.renderSqaure(3)}
+            {this.renderSqaure(4)}
+            {this.renderSqaure(5)}
+        </div>
+        <div className='board-row'>
+            {this.renderSqaure(6)}
+            {this.renderSqaure(7)}
+            {this.renderSqaure(8)}
+        </div>
+      </div>
+    )
+  }
+}
