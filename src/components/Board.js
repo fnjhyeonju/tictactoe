cf60d36cef2ee45568ca5b02cee9871a7b8e23c8
hyeonju <hyeonju@fnj.or.kr>
2024-03-28 23:21:43 +0900

    React Hooks를 이용해 함수형 컴포넌트로 바꾸기

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,47 +1,87 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-export default class Board extends Component {
+const Board = ()=>{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      squares : Array(9).fill(null),
-    }
-    console.log(this.state);
+  const [squares,setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i)=>{
+    const newSquares = squares.slice(); //원본 배열을 바꾸지 않기위해서?
+    newSquares[i] = 'X';
+    setSquares(newSquares);
   }
 
-  renderSqaure(i){
-    return <Square value={this.state.squares[i]} 
-              onClick={()=>{this.handleClick(i); }}
-            />
+  const renderSqaure = (i) => {
+    return <Square value={squares[i]} 
+              onClick={()=>{handleClick(i); }}/>
   }
-  handleClick(i){
-    const squares = this.state.squares.slice(); //원본 배열을 바꾸지 않기위해서?
-    squares[i] = 'X';
-    this.setState({squares : squares});
-  }
-  render() {
-    return (
-      <div>
-        <div className='status'></div>
-        <div className='board-row'>
-            {this.renderSqaure(0)}
-            {this.renderSqaure(1)}
-            {this.renderSqaure(2)}
-        </div>
-        <div className='board-row'>
-            {this.renderSqaure(3)}
-            {this.renderSqaure(4)}
-            {this.renderSqaure(5)}
-        </div>
-        <div className='board-row'>
-            {this.renderSqaure(6)}
-            {this.renderSqaure(7)}
-            {this.renderSqaure(8)}
-        </div>
+  
+  return (
+    <div>
+      <div className='status'></div>
+      <div className='board-row'>
+          {renderSqaure(0)}
+          {renderSqaure(1)}
+          {renderSqaure(2)}
       </div>
-    )
-  }
+      <div className='board-row'>
+          {renderSqaure(3)}
+          {renderSqaure(4)}
+          {renderSqaure(5)}
+      </div>
+      <div className='board-row'>
+          {renderSqaure(6)}
+          {renderSqaure(7)}
+          {renderSqaure(8)}
+      </div>
+    </div>
+  )
+  
 }
+
+export default Board
+
+// export default class Board extends Component {
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       squares : Array(9).fill(null),
+//     }
+//     console.log(this.state);
+//   }
+
+//   renderSqaure(i){
+//     return <Square value={this.state.squares[i]} 
+//               onClick={()=>{this.handleClick(i); }}
+//             />
+//   }
+//   handleClick(i){
+//     const squares = this.state.squares.slice(); //원본 배열을 바꾸지 않기위해서?
+//     squares[i] = 'X';
+//     this.setState({squares : squares});
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <div className='status'></div>
+//         <div className='board-row'>
+//             {this.renderSqaure(0)}
+//             {this.renderSqaure(1)}
+//             {this.renderSqaure(2)}
+//         </div>
+//         <div className='board-row'>
+//             {this.renderSqaure(3)}
+//             {this.renderSqaure(4)}
+//             {this.renderSqaure(5)}
+//         </div>
+//         <div className='board-row'>
+//             {this.renderSqaure(6)}
+//             {this.renderSqaure(7)}
+//             {this.renderSqaure(8)}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,32 @@
 import { Component } from "react";
 import "./Square.css";
 
-export default class Square extends Component{
-
-    render(){
-        return(
-            <button className="square"
-                onClick={()=>{this.props.onClick()}}
-            >
-                {this.props.value}
-            </button>
-        )
-    }
+const Square = (props)=>{
+    return(
+        <button className="square"
+            onClick={()=>{props.onClick()}}>
+        {props.value}
+        </button>
+    )
 }
+// const Square = ({onClick,value})=>{
+//     return(
+//         <button className="square"
+//             onClick={onClick}>
+//         {value}
+//         </button>
+//     )
+// }
+export default Square
+// export default class Square extends Component{
+
+//     render(){
+//         return(
+//             <button className="square"
+//                 onClick={()=>{this.props.onClick()}}
+//             >
+//                 {this.props.value}
+//             </button>
+//         )
+//     }
+// }
