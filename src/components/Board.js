d2a1723e9b5647ed1adc5c87000fe48869cd797f
hyeonju <hyeonju.dlf@gmail.com>
2024-03-30 12:40:25 +0900

    시간여행 추가하기 / 스타일링

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,62 +1,18 @@
-import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-const Board = ()=>{
-
-  const [squares,setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const calculateWinner = (squares) =>{
-    const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for(let i=0; i<lines.length; i++){
-      const [a,b,c] = lines[i];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-        return squares[a];
-      }
-    }
-
-    return null;
-  }
-  let status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
-  const winner = calculateWinner(squares);
-  if(winner){
-    status = `Winner : ${winner}`;
-  }else{
-    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
-  }
-
-  const handleClick = (i)=>{
-    const newSquares = squares.slice(); //원본 배열을 바꾸지 않기위해서?
-
-    if(newSquares[i] || calculateWinner(newSquares)){
-      return;
-    }
-    newSquares[i] = xIsNext ? 'X' : 'O' ;
-    setSquares(newSquares);
-    //setXIsNext(!xIsNext);
-    setXIsNext(prev => !prev);
-  }
+const Board = ({squares,onClick})=>{
 
   const renderSqaure = (i) => {
     return <Square value={squares[i]} 
-              onClick={()=>{handleClick(i); }}/>
+              onClick={()=>{onClick(i); }}/>
   }
 
 
   
   return (
-    <div>
-      <div className='status'>{status}</div>
+    <div className='board-wrapper'>
+
       <div className='board-row'>
           {renderSqaure(0)}
           {renderSqaure(1)}
